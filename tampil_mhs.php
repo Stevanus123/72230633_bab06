3a8ee18127649c3a7e2242fa21fefd14fe6f9e70
Stevanus Denko <stevanusdenko46@gmail.com>
2025-03-12 22:48:56 +0700

    new

--- tampil_mhs.php
+++ tampil_mhs.php
@@ -8,17 +8,64 @@
 </head>
 
 <body>
+    <?php
+    include("koneksi.php");
+    $baris = $conn->query("SELECT * FROM mahasiswa")->num_rows;
+    $fill = 2;
+    $max_page = ceil($baris / $fill) -1;
+    if (!empty($_GET)) {
+        $page = $_GET["navi"];
+        switch ($page) {
+            case "<<":
+                $page = 0;
+                break;
+            case "<":
+                if($_GET["page"] > 0)
+                    $page = $_GET["page"] - 1;
+                else
+                    $page = 0;
+                break;
+            case ">":
+                if($_GET["page"] < $max_page)
+                    $page = $_GET["page"] + 1;
+                else
+                    $page = $max_page;
+                break;
+            case ">>":
+                $page = $max_page;
+                break;
+        }
+        $mulai = $page * $fill;
+    } else{
+        $page = 0;
+        $mulai = 0;
+    }
+    ?>
+
     <h2>Daftar Mahasiswa</h2>
-    <table border="1">
-        <?php
-        include("koneksi.php");
-        include("fungsi.php");
-        $sql = "SELECT * FROM mahasiswa";
-        $result = $conn->query($sql);
-        while ($row = $result->fetch_assoc())
-            card($row);
-        ?>
-    </table>
+    <form>
+        <table>
+            <tr>
+                <td>Halaman ke-</td>
+                <td><input type="text" name="page" value="<?= $page ?>" readonly></td>
+            </tr>
+            <?php
+            include("fungsi.php");
+            $sql = "SELECT * FROM mahasiswa LIMIT $mulai, $fill";
+            $result = $conn->query($sql);
+            while ($row = $result->fetch_assoc())
+                card($row);
+            ?>
+            <tr>
+                <td>
+                    <input type="submit" name="navi" value="<<">
+                    <input type="submit" name="navi" value="<">
+                    <input type="submit" name="navi" value=">">
+                    <input type="submit" name="navi" value=">>">
+                </td>
+            </tr>
+        </table>
+    </form>
 </body>
 
 </html>
